867818aac274299e5b39241f5af4a71028b78391
n0thing233 <adamli19891123@gmail.com>
2021-01-06 14:25:22 -0800

    Update shopping_pattern.py

--- noodlewhale/amazon/OA/shopping_pattern.py
+++ noodlewhale/amazon/OA/shopping_pattern.py
@@ -1,6 +1,6 @@
 #https://aonecode.com/amazon-online-assessment-shopping-patterns
-#O(n**3)
-#O(n**2)
+#O(V+E)
+#O(V+E)
 def get_min(num_node,num_edge,product_from,product_to):
     pair_dict = {}
     trio_set = set()
@@ -22,16 +22,11 @@ def get_min(num_node,num_edge,product_from,product_to):
                         trio_set.add(tuple(sorted([i,pair_dict[i][j],pair_dict[i][j+k]])))
                     k += 1
     res = float("inf")
+    print(trio_set)
     for trio in trio_set:
         a,b,c = trio[0], trio[1], trio[2]
         my_sum = 0
-        external_set = set()
         for i in [a,b,c]:
-            for j in pair_dict[i]:
-                external_set.add(j)
-        my_sum += len(external_set) - 3
+            my_sum += len(pair_dict[i])-2
         res = min(my_sum,res)
     return res
-
-#test case:
-print(get_min(num_node = 6, num_edge = 6, product_from = [1,2,2,3,4,5], product_to = [2,4,5,5,5,6]))
