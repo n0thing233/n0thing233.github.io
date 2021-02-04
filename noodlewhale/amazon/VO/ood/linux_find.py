e9f362ec3c6d21bf81f53a4510dbb04069b690c0
n0thing233 <adamli19891123@gmail.com>
2021-02-03 22:14:43 -0800

    Update linux_find.py

--- noodlewhale/amazon/VO/ood/linux_find.py
+++ noodlewhale/amazon/VO/ood/linux_find.py
@@ -1,2 +1,39 @@
+from abc import ABC, abstractmethod
 class File:
-  
+  def __init__(self,
+               name = None,
+               creation_timestamp = None,
+               size = None,
+               isdir = None,
+               children = None):
+      self.filename = filename
+      self.creation_timestamp = creation_timestamp
+      self.size = size
+      self.isdir = isdir
+      self.children = []
+
+class filter(ABC):
+  @abstractmethod
+  def apply(self,file)：
+      pass
+class sizeFilter(filter):
+  def apply(self,file,size):
+      if file.size >= size:
+          return True
+      else:
+          return False
+class suffixFilter(filter):
+   def apply(self,file,suffix):
+       return file.endswith(suffix)
+
+class findcommand:
+  def findWithFilter(self,file,filter):
+      res = []
+      if  not file.isdir:
+        raise Exception("file not directory!")
+      for i in file.children:
+        if i.isdir:
+          res += findWithFilter(i,filter)
+        elif filter.apply(i):
+          res += file
+      return res
