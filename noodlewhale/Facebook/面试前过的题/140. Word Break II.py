7744b468056020371e6beb21934f5373d9e7ec0e
n0thing233 <adamli19891123@gmail.com>
2021-05-17 22:34:44 -0700

    Create 140. Word Break II.py

---
+++ noodlewhale/Facebook/面试前过的题/140. Word Break II.py
@@ -0,0 +1,22 @@
+#top-down dp with memo,think from insert break. it will be n^2+ 2^n "aaaaaaaaaaa" "a" "aa" etc
+#time:O(S) = O(S-1)+n
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        if not s:
+            return []
+        if not wordDict:
+            return []
+        memo = {}
+        def helper(s):
+            if s in memo:
+                return memo[s]
+            if s == '':
+                return [[]]
+            res = []
+            for word in wordDict:
+                if s.startswith(word):
+                    for i in helper(s[len(word):]):
+                        res.append([word]+i)
+            memo[s] = res
+            return res
+        return [' '.join(i) for i in helper(s)]
